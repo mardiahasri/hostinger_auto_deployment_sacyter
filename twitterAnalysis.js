e444ad7655b31fb41c66a7a04e1793d69e33f8ea
mardiahasri <45283249+mardiahasri@users.noreply.github.com>
2021-02-07 09:12:24 -0500

    Add files via upload

---
+++ twitterAnalysis.js
@@ -0,0 +1,68 @@
+/*
+ By: Andre Fischbacher
+ Date: 2017-03-17
+ */
+
+var twitterAnalysis = function () {
+    var async = require('async');
+    var twitter = require("twitter");
+    var dotenv = require("dotenv");
+    var sentiment = require("sentiment");
+
+    dotenv.config();
+
+    var twitterApi = new twitter({
+        consumer_key: "dB5majovvOcoIb9EN3RygPxTW",
+        consumer_secret: "hO0V9ven0OmyDQzqolcYawKW8jA8nYG0B39MsWlnq7nVvnZswt",
+        access_token_key: "757231087214964737-rfdYzMfx3k6RR00Ov8WGlT9ukPCVaMj",
+        access_token_secret: "Og9iB549Z22IObpOzAYPUbzVtfI0BqgyXMNNSejiFkaJW"
+    });
+
+
+    this.getTwitterHashTagData = function (query, callback) {
+        var dataScore = {"Very Negative": 0, "Negative": 0, "Neutral": 0, "Positive": 0, "Very Positive": 0};
+        var sum = 0;
+
+        twitterApi.get("search/tweets", {q: "#" + query, lang: "en"}, function (error, tweets, response) {
+            var twitterData = [];
+            var sortedTwitterData = [];
+            if (error) callback(error);
+
+            async.each(tweets.statuses, function (item, callEach) {
+
+                twitterData.push(item.text);
+                var sentScore = sentiment(item.text, function (err, data) {
+                    if (data.score < -4) {
+                        sortedTwitterData.unshift(item.text);
+                        dataScore["Very Negative"] += 1;
+                    }
+                    else if (data.score >= -3 && data.score < 0) {
+                        sortedTwitterData.splice(2, 0, item.text);
+                        dataScore["Negative"] += 1;
+                    }
+                    else if (data.score == 0) {
+                        sortedTwitterData.splice(3, 0, item.text);
+                        dataScore["Neutral"] += 1;
+                    }
+                    else if (data.score > 0 && data.score <= 3) {
+                        sortedTwitterData.splice(4, 0, item.text);
+                        dataScore["Positive"] += 1;
+                    }
+                    else {
+                        sortedTwitterData.push(item.text);
+                        dataScore["Very Positive"] += 1;
+                    }
+                    callEach(); // decrementing by 1 until the length of the array is completed for the tweets.statuses
+                });
+
+            }, function () {
+
+                callback(null, dataScore, sortedTwitterData);
+
+            });
+
+        });
+    };
+};
+
+module.exports = twitterAnalysis;
